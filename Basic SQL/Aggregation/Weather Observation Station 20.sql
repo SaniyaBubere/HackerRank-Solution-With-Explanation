e324cd6d1da911c386c5e4b35b30397191de5155
Saniya Bubere <116152917+SaniyaBubere@users.noreply.github.com>
2023-08-10 13:37:35 +0530

    Create Weather Observation Station 20.sql

---
+++ Basic SQL/Aggregation/Weather Observation Station 20.sql
@@ -0,0 +1,10 @@
+/* A median is defined as a number separating the higher half of a data set from the lower half. 
+Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
+
+*/
+
+
+-- solution in Oracle DB
+
+
+select round(median(lat_n),4) from station;
